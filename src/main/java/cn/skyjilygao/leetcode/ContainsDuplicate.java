58078e21846ddd19e38141d551bb9bf75a932e40
Administrator <gaoqihua@empowerwin.com>
2020-09-18 16:54:10 +0800

    217. 存在重复元素 增加题解

--- src/main/java/cn/skyjilygao/leetcode/ContainsDuplicate.java
+++ src/main/java/cn/skyjilygao/leetcode/ContainsDuplicate.java
@@ -15,11 +15,14 @@ public class ContainsDuplicate {
         int[] nums = new int[]{1,2,3,4,5,5,6,7};
         // 使用双枢快速排序
         System.out.println(containsDuplicate(nums));
-        // 使用set集合
+        // 使用set集合，先统计所有数据再比较大小
+        System.out.println(containsDuplicate2(nums));
+        // 使用set集合，是否有重复
         System.out.println(containsDuplicate1(nums));
         // 原始方法
         System.out.println(containsDuplicate0(nums));
     }
+
 
     /**
      * 使用 ${@link Arrays#sort(int[])} 双枢快速排序。最坏时间复杂度为O(n * log^n)
@@ -36,6 +39,22 @@ public class ContainsDuplicate {
             }
         }
         return false;
+    }
+
+    /**
+     * 直接统计不重复后count跟nums长度比较是否相等
+     * @param nums
+     * @return
+     */
+    public static boolean containsDuplicate2(int[] nums) {
+        // 法1：利用set不可重复属性
+        /*Set<Integer> set = new HashSet<>(nums.length);
+        for (int num : nums) {
+            set.add(num);
+        }
+        return set.size() != nums.length;*/
+        // 法2：stream的distinct就是统计不重复的数据集合，count就是集合的大小。
+        return Arrays.stream(nums).distinct().count() != nums.length;
     }
 
     /**
@@ -61,7 +80,8 @@ public class ContainsDuplicate {
      */
     public static boolean containsDuplicate0(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 1; j < nums.length; j++) {
+            // j = i + 1而不是j = 1，因为只需要跟后面毕竟就可以。不用每次都从头开始比较
+            for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] == nums[j]) {
                     return true;
                 }
@@ -69,5 +89,4 @@ public class ContainsDuplicate {
         }
         return false;
     }
-
 }
